a5f0352065d3989f631251fcc66e1b7c72a91027
RICARDO <ricardogarciaherbas@gmail.com>
2024-10-08 22:06:42 -0400

    Modificacion de navbar y paginas

---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+  return <h1>Informacion acerca de nosotros</h1>;
+}
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Contact() {
+  return <h1>Contactate con nosotros</h1>;
+}
+
+export default Contact;
